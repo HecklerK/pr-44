4fd03987ec937c854ac96384186fd7cf343101b6
HecklerK <49901822+HecklerK@users.noreply.github.com>
2019-11-18 10:41:29 +0300

    Add files via upload

---
+++ pr44/pr44/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr44
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPage(pages.login);
+        }
+
+        public enum pages
+        {
+            login,
+            regin
+        }
+
+        void OpenPage(pages pages)
+        {
+            if (pages == pages.login) frame.Navigate(new login(this));
+            else if (pages == pages.regin) frame.Navigate(new regin(this));
+        }
+
+        public DataTable Select(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");
+            SqlConnection sqlConnection = new SqlConnection("server=KB37-122-C11\\SQLEXPRESS; Trusted_Connection=Yes; DataBase=AviaProm");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
---
+++ pr44/pr44/regin.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr44
+{
+    /// <summary>
+    /// Логика взаимодействия для regin.xaml
+    /// </summary>
+    public partial class regin : Page
+    {
+        public MainWindow mainWindow;
+        public regin(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+        private void return_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.frame.Navigate(new login(mainWindow));
+        }
+
+        private void regin_Click(object sender, RoutedEventArgs e)
+        {
+            if (t_login.Text.Length > 0)
+            {
+                if (t_pass.Password.Length > 0)
+                {
+                    if (t_pass_r.Password.Length > 0)
+                    {
+                        string[] dataLogin = t_login.Text.Split('@');
+                        if (dataLogin.Length == 2)
+                        {
+                            string[] data2Login = dataLogin[1].Split('.');
+                            if (data2Login.Length == 2)
+                            {
+                                
+                            }
+                            else MessageBox.Show("Укажите логин в форме x@x.x");
+                        }
+                        else MessageBox.Show("Укажите логин в форме x@x.x");
+                    }
+                    else MessageBox.Show("Повторите пароль");
+                }
+                else MessageBox.Show("Введите пароль");
+            }
+            else MessageBox.Show("Введите логин");
+        }
+    }
+}
